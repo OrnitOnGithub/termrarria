487dd62918607354e61e1ffda9601b52f9a9602d
ornit <ornithopterthethopter@gmail.com>
2024-05-12 10:57:33 +0200

    Game kinda works now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use colored::{CustomColor, Colorize};
-use std::{sync::mpsc, time::Instant};
+use std::time::Instant;
 use console::Term;
 
 /// whether to show debug info in the game
@@ -9,69 +9,94 @@ const DEBUG_INFO: bool = true;
 /// Gets multiplied by delta time and added to position every frame
 static mut MOVEMENT_DIRECTION: Vector2 = Vector2::const_new();
 
+/// Position of top left corner of camera's view.
+static mut CAMERA_POSITION: Vector2 = Vector2::const_new();
 static mut PLAYER_POSITION: Vector2 = Vector2::const_new();
+
+const WORLD_SIZE_X: usize = 50;
+const WORLD_SIZE_Y: usize = 50;
+const WORLD_SAVE_PATH: &str = "world.save";
+
+const SCREEN_SIZE_X: usize = 40;
+const SCREEN_SIZE_Y: usize = 40;
 
 /// We need a minimum frame time because if we draw too fast to terminal the "image"
 /// will appear torn or incomplete <br>
 /// 10 milliseconds (100fps (actually 90fps)) appears to be the bare minimum,
 /// at least on my laptop with Konsole
-const MIN_FRAME_TIME: u128 = 10_000; // in microseconds
+const MIN_FRAME_TIME: u128 = 50_000; // in microseconds
 
 /// static variable that holds time frame time
 static mut DELTA_TIME: u128 = 100_000;
 
 fn main() {
-  // initiate the screen
-  let mut screen: Screen = Screen { pixels: Vec::new(), size_x: 40, size_y: 20 };
-  screen.init();
   
-  // main thread, the game thread. mostly draws to screen and calculated delta time
+  // main thread, the game thread. mostly draws to screen and calculates delta time
+  // MARK: Game thread
   let game_thread = std::thread::spawn(move || {
+    // INITIATE the screen
+    let mut screen: Screen = Screen { pixels: Vec::new(), size_x: SCREEN_SIZE_X, size_y: SCREEN_SIZE_Y };
+
+    // LOAD the world
+    let unrendered_world = load_world(WORLD_SAVE_PATH);
 
     // initialise frame start and end times to be used to calculate delta_time (frame time)
     let mut frame_start_time: Instant = Instant::now();
     let mut frame_end_time: Instant = Instant::now();
+    
     loop /* game loop */ {
-
       // calculate frame time
       unsafe {DELTA_TIME = (frame_end_time - frame_start_time).as_micros();}
-
       // set frame start time for this frame
       frame_start_time = Instant::now();
-      
-      // draw player
+
+      // clear the screen
+      screen.init();
+
+      // RENDER the world
+      let mut rendered_world: Vec<Square> = Vec::new();
+      // as of now, basic rendering. No shading applied. Uses the default `new()` function to generate colours and ascii art.
+      for unrendered_square in unrendered_world.clone() {
+        rendered_world.push(Square::new(unrendered_square));
+      }
+
+      screen = render_world(screen, rendered_world);
+
+      // PLAYER
       unsafe {
-        let player_x: i16 = PLAYER_POSITION.x;
-        let player_y: i16 = PLAYER_POSITION.y;
+        // update player pos
+        CAMERA_POSITION.x += MOVEMENT_DIRECTION.x;
+        CAMERA_POSITION.y += MOVEMENT_DIRECTION.y;
+        // draw player
+        let player_x: i16 = CAMERA_POSITION.x;
+        let player_y: i16 = CAMERA_POSITION.y;
         screen.pixels[player_y as usize][player_x as usize] = Square::new(SquareType::Player);
       }
-      
+
       // show debug info
       if DEBUG_INFO {
         unsafe {
-          println!("frame time : {:?}μs", DELTA_TIME);
+          println!("frame time : {:?}μs",  DELTA_TIME              );
           println!("frame rate : {:?}fps", 1_000_000/(DELTA_TIME+1));
-          println!("player pos : {:?}", PLAYER_POSITION);
-          println!("velocity   : {:?}", MOVEMENT_DIRECTION);
+          println!("camera pos : {:?}",    CAMERA_POSITION         );
+          println!("velocity   : {:?}",    MOVEMENT_DIRECTION      );
         }
       }
 
       // draw main screen to terminal
       screen.draw();
 
-      // only end the frame if we're beyond minimum frame time
-      loop /* frame wait loop */ {
-        if frame_start_time.elapsed().as_micros() > MIN_FRAME_TIME {
-          clearscreen::clear().expect("failed to clear screen");
-          break;
-        }
+      // sleep until we reach min frame time
+      if frame_start_time.elapsed().as_millis() < MIN_FRAME_TIME {
+        std::thread::sleep(std::time::Duration::from_micros((MIN_FRAME_TIME - frame_start_time.elapsed().as_millis()) as u64));
       }
+      clearscreen::clear().expect("failed to clear screen");
       frame_end_time = Instant::now();
     }
   });
     
+  // MARK: Input thread
   let input_thread = std::thread::spawn(|| {
-
     
     loop {
       let key = Term::buffered_stdout().read_key().expect("idc");
@@ -80,6 +105,7 @@ fn main() {
         println!("PRESSED: {:?}", key);
       }
       let mut horizontal_movement: i16 = 0;
+      let mut vertical_movement: i16 = 0;
       // unsafe because static mutation (why is this unsafe?)
       match key {
 
@@ -89,12 +115,18 @@ fn main() {
         console::Key::Char('d') => {
           horizontal_movement = 1;
         }
-
+        console::Key::Char('w') => {
+          vertical_movement = -1;
+        }
+        console::Key::Char('s') => {
+          vertical_movement = 1;
+        }
         _ => {
           horizontal_movement = 0;
         }
       }
       unsafe { MOVEMENT_DIRECTION.x = horizontal_movement};
+      unsafe { MOVEMENT_DIRECTION.y = vertical_movement};
 
     }
   });
@@ -108,6 +140,7 @@ fn main() {
       // so we solve the issue caveman style by periodically setting x movement direction back to 0
       unsafe {std::thread::sleep(std::time::Duration::from_micros(DELTA_TIME as u64 * 5));}
       unsafe {MOVEMENT_DIRECTION.x = 0;}
+      unsafe {MOVEMENT_DIRECTION.y = 0;}
     }
   });
 
@@ -117,6 +150,7 @@ fn main() {
   let _ = reset_thread.join();
 }
 
+// MARK: World load/render
 /// Loads the world into a vector of SquareTypes.
 fn load_world(save_path: &str) -> Vec<SquareType> {
   let contents: String = std::fs::read_to_string(save_path)
@@ -134,6 +168,26 @@ fn load_world(save_path: &str) -> Vec<SquareType> {
   return square_type_list;
 }
 
+fn render_world(mut screen: Screen, square_list:Vec<Square>) -> Screen {
+  // for x in player_pos_x -> +windowsize
+  // for y in player_pos_< -> +windowsize
+  // block_at_pos = blocklist[x * y]
+  // screen[x][y] = block_at_pos
+  unsafe {
+    for x in CAMERA_POSITION.x..(SCREEN_SIZE_X as i16 + CAMERA_POSITION.x as i16) {
+      for y in CAMERA_POSITION.y..(SCREEN_SIZE_Y as i16 + CAMERA_POSITION.y as i16) {
+        if y < WORLD_SIZE_Y as i16 && x < WORLD_SIZE_X as i16 {
+          let square_to_draw = square_list[((x)+(y * WORLD_SIZE_Y as i16)) as usize].clone();
+          screen.pixels[(y - CAMERA_POSITION.y) as usize][(x - CAMERA_POSITION.x) as usize] = square_to_draw;
+        }
+      }
+    }
+  }
+
+  return screen;
+}
+
+// MARK: Structs
 #[derive(Debug,Clone,Copy)]
 struct Vector2 {
   x: i16,
